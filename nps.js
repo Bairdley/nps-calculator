be5b1f073f93e6dcec98e9e9fd6f689e514d0a53
Bairdley <bradley.r.mitchell@gmail.com>
2016-03-30 21:27:40 -0500

    Update nps.js

--- nps.js
+++ nps.js
@@ -1,6 +1,8 @@
-var inputs = document.getElementsByTagName('input');
+var inputs = document.getElementsByTagName('input'),
+    displays = document.getElementsByTagName('span');
+
 inputs[3].addEventListener('input', function() {
-    document.getElementById('goal-value').innerHTML = inputs[3].value;
+    displays[0].innerHTML = inputs[3].value;
 })
 
 for (var i = 0; i < inputs.length; i++) {
@@ -11,37 +13,37 @@ for (var i = 0; i < inputs.length; i++) {
 
 function calculations() {
     //NPS calculation
-    var promoters = Number(document.getElementById('one').value),
-        neutrals = Number(document.getElementById('two').value),
-        detractors = Number(document.getElementById('three').value),
+    var promoters = Number(inputs[0].value),
+        neutrals = Number(inputs[1].value),
+        detractors = Number(inputs[2].value),
         total = promoters + neutrals + detractors,
         nps = (((promoters - detractors) / total) * 100).toFixed(2);
   
-    var p = document.getElementsByTagName('span')[1];
+    var currentNPS = displays[1];
     if (nps <= 100) {
-        p.innerHTML = nps;
+        currentNPS.innerHTML = nps;
     }
   
     //NPS color-change
-    nps < 50 ? p.style.color = '#EDE275' : p.style.color = 'seagreen';
+    nps < 50 ? currentNPS.style.color = '#EDE275' : currentNPS.style.color = 'seagreen';
   
     //NPS goal calculation
-    var d = Number(document.getElementById('four').value);
-    document.getElementsByTagName('span')[0].innerHTML = d;
-    var x = (((-100 * detractors) - (detractors * d) + 
-        (100 * promoters) - (neutrals * d) - 
-        (promoters * d)) / (-100 + d)).toFixed(2);
-    document.getElementsByTagName('span')[2].innerHTML = x;
+    var pToGoal = Number(document.getElementById('four').value);
+    displays[0].innerHTML = pToGoal;
+    var x = (((-100 * detractors) - (detractors * pToGoal) + 
+              (100 * promoters) - (neutrals * pToGoal) - 
+              (promoters * pToGoal)) / (-100 + pToGoal)).toFixed(2);
+    displays[2].innerHTML = x;
   
     //Fringe-case conditionals
-    switch (true) {
-        case (x < 0): 
-            document.getElementsByTagName('span')[2].innerHTML = '0.00';
-            break;
-        case (promoters + neutrals + detractors === 0): 
-            document.getElementsByTagName('span')[2].innerHTML = '1.00';
-            p.innerHTML = '0.00';
-            break;
+     switch (true) {
+      case (x < 0): 
+        displays[2].innerHTML = '0.00';
+        break;
+      case (promoters + neutrals + detractors === 0): 
+        displays[2].innerHTML = '1.00';
+        currentNPS.innerHTML = '0.00';
+        break;
     } 
 };
 
@@ -51,8 +53,9 @@ document.getElementById('clear').addEventListener('click', function() {
         inputs[i].value = 0;     
     }
     inputs[3].value = 80;
-    document.getElementById('goal-value').innerHTML = '80';
-    document.getElementsByTagName('span')[1].innerHTML = '0.00';
-    document.getElementsByTagName('span')[2].innerHTML = '1.00';
-    document.getElementsByTagName('span')[1].style.color = 'seagreen';
+    displays[0].innerHTML = '80';
+    displays[1].innerHTML = '0.00';
+    displays[1].style.color = 'seagreen';
+    displays[2].innerHTML = '1.00';
 });
+
